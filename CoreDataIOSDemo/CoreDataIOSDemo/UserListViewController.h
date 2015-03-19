3ff5e1dc2e2b3e401c8f47fcf9b933271e17e4f6
Rohit Dhiman <sp.rohit385@gmail.com>
2015-03-19 16:23:12 +0200

    .gitignore is working

---
+++ CoreDataIOSDemo/CoreDataIOSDemo/UserListViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserListViewController.h
+//  CoreDataIOSDemo
+//
+//  Created by Rohit Dhiman  on 14/08/14.
+//  Copyright (c) 2014 Anchanto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserListViewController : UIViewController
+
+@end
---
+++ CoreDataIOSDemo/CoreDataIOSDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataIOSDemo
+//
+//  Created by Rohit Dhiman  on 14/08/14.
+//  Copyright (c) 2014 Anchanto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
