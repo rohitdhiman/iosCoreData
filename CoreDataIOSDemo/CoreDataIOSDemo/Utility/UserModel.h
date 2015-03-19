1f3488722119b458908080580a80a54ecf09f644
Rohit Dhiman <venkatmanohar@venkatmharsmini.in.ibm.com>
2015-03-19 13:14:37 +0200

    UserModel added in utility

---
+++ CoreDataIOSDemo/CoreDataIOSDemo/Utility/UserModel.h
@@ -0,0 +1,18 @@
+//
+//  UserModel.h
+//  CoreDataIOSDemo
+//
+//  Created by Rohit Dhiman on 19/03/15.
+//  Copyright (c) 2015 Anchanto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserModel : NSObject
+
+@property (nonatomic, strong) NSString *userFirstName;
+@property (nonatomic, strong) NSString *userLastName;
+@property (nonatomic, strong) NSString *userDeptName;
+
+
+@end
---
+++ CoreDataIOSDemo/CoreDataIOSDemo/Utility/UserModel.m
@@ -0,0 +1,15 @@
+//
+//  UserModel.m
+//  CoreDataIOSDemo
+//
+//  Created by Rohit Dhiman on 19/03/15.
+//  Copyright (c) 2015 Anchanto. All rights reserved.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+@synthesize userFirstName;
+@synthesize userLastName;
+@synthesize userDeptName;
+@end
